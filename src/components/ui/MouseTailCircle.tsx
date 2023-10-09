00eccf28d28f5ce1f0a2fc6782a3b49c7bf3515a
pinky-pig <2545589993@qq.com>
2023-10-09 11:39:28 +0800

    fix: add MouseTailCircle

--- src/components/ui/MouseTailCircle.tsx
+++ src/components/ui/MouseTailCircle.tsx
@@ -11,51 +11,32 @@ export default function MouseTail() {
     if (Number.isNaN(clientX) || Number.isNaN(clientY)) {
       return
     } else {
-      setPoints([...points, [clientX, clientY]].splice(-20))
+      setPoints([...points, [clientX, clientY]])
     }
   }, [clientX, clientY])
 
 
-  // 2. 鼠标停止移动，点集也要删除
-  React.useEffect(() => {
-    loop()
-  }, [])
-
-  let timestamp = 0
-  function loop() {
-    const now = Date.now()
-    const elapsed2 = now - timestamp
-    if (elapsed2 > 32) {
-      if (points.length > 1) { 
-        const temp = points.slice(0, Math.ceil(points.length * 0.1))
-        setPoints(temp)
+  // 2. 定义 loop 函数，鼠标停止移动，点集也要删除
+  const loopRef = React.useRef<() => void>();
+  let timestamp = React.useRef<number>(0);
+  loopRef.current = () => {
+      const now = Date.now();
+      const elapsed2 = now - timestamp.current;
+      if (elapsed2 > 32) {
+        if (points.length > 1) {
+          setPoints((points) => {
+            const temp = [...points];
+            temp.splice(0, Math.ceil(points.length * 0.1));
+            return temp;
+          });
+        }
+        timestamp.current = now;
       }
-    }
-    requestAnimationFrame(loop)
-  }
-
- 
-
-  // 定义 loop 函数
-  // const loopRef = React.useRef<() => void>();
-  // loopRef.current = () => {
-  //   const now = Date.now();
-  //   const elapsed2 = now - timestamp;
-  //   if (elapsed2 > 32) {
-  //     console.log(points);
-  //     if (points.length > 1) {
-  //       const temp = points.slice(0, Math.ceil(points.length * 0.1));
-  //       setPoints(temp);
-  //     }
-  //     timestamp = now;
-  //   }
-  //   requestAnimationFrame(loopRef.current!);
-  // };
-
-  // let timestamp = 0;
-  // React.useEffect(() => {
-  //   loopRef.current!();
-  // }, [points]);
+      requestAnimationFrame(loopRef.current!);
+  };
+  React.useEffect(() => {
+    loopRef.current!();
+  }, []);
 
   return (
     <div className="fixed top-0 left-0 bottom-0 right-0 w-full h-full pointer-events-none">
@@ -67,7 +48,7 @@ export default function MouseTail() {
               transform: ` translate( calc(-50% + ${point[0]}px), calc(-50% + ${point[1]}px) ) `,
               willChange: 'transform'
             }}
-            className="pointer-events-none bg-white bg-opacity-10 -translate-1/2 w-160px h-160px rounded-full bg-transparent absolute top-0 left-0 "
+            className="pointer-events-none bg-white bg-opacity-10 -translate-1/2 w-160px h-160px rounded-full absolute top-0 left-0 "
           />
         ))
       }
@@ -77,7 +58,7 @@ export default function MouseTail() {
           transform: `translate(calc(-50% + ${clientX}px), calc(-50% + ${clientY}px))`,
           willChange: 'transform'
         }}
-        className="pointer-events-none  w-160px h-160px rounded-full bg-transparent absolute top-0 left-0 border-2 border-white border-solid"
+        className="pointer-events-none w-160px h-160px rounded-full bg-transparent absolute top-0 left-0 border-2 border-white border-solid"
       />
     </div>
   )
