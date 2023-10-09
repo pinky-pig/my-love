b1909e661f9c9c823fc1009e655b68ad91881011
pinky-pig <2545589993@qq.com>
2023-10-09 17:20:44 +0800

    fix: MouseTail key

--- src/components/ui/MouseTailCircle.tsx
+++ src/components/ui/MouseTailCircle.tsx
@@ -7,11 +7,37 @@ export default function MouseTailCircle() {
 
   // 1. 监听鼠标位置，设置点集
   React.useEffect(() => {
-    // eslint-disable-next-line no-empty
-    if (Number.isNaN(clientX) || Number.isNaN(clientY)) { }
-    else {
-      setPoints([...points, [clientX, clientY]])
+    if (!Number.isNaN(clientX) && !Number.isNaN(clientY)) {
+      if (points.length === 0) {
+        setPoints([...points, [clientX, clientY]])
+      }
+      else {
+        const result = []
+        const start = points[points.length - 1]
+        const end = [clientX, clientY]
+        // 1. 计算两点之间的距离
+        const d = Math.sqrt(
+          Math.abs(end[0] - start[0]) ** 2
+          + Math.abs(end[1] - start[1]) ** 2,
+        )
+        if (d > 10) {
+          // 2. 调整步数以控制插值的密度
+          const steps = Math.floor(d / 10)
+          for (let j = 0; j < steps; j++) {
+            const x = lerp(start[0], end[0], j / steps)
+            const y = lerp(start[1], end[1], j / steps)
+            result.push([x, y])
+          }
+        }
+        // 3. 将差值点位填充
+        setPoints([...points, ...result, [clientX, clientY]])
+      }
     }
+
+    // if (Number.isNaN(clientX) || Number.isNaN(clientY)) { }
+    // else {
+    //   setPoints([...points, [clientX, clientY]])
+    // }
   }, [clientX, clientY])
 
   // 2. 创建循环，拖尾效果
@@ -39,17 +65,23 @@ export default function MouseTailCircle() {
     requestAnimationFrame(loop)
   }
 
+  // 定义lerp函数
+  function lerp(start: number, end: number, t: number) {
+    return start + (end - start) * t
+  }
+
   return (
     <div className="fixed top-0 left-0 bottom-0 right-0 w-full h-full pointer-events-none">
       {
         points.map((point, index) => (
           <div
-            key={`${point[0]}_circle_${point[1]}`}
+            key={index}
             style={{
               transform: ` translate( calc(-50% + ${point[0]}px), calc(-50% + ${point[1]}px) ) `,
               willChange: 'transform',
+              background: index > points.length - 5 ? 'rgba(255, 255, 255, 0.05)' : 'rgba(255, 255, 255, 0.1)',
             }}
-            className="pointer-events-none bg-white bg-opacity-10 -translate-1/2 w-160px h-160px rounded-full absolute top-0 left-0 "
+            className="pointer-events-none  -translate-1/2 w-160px h-160px rounded-full absolute top-0 left-0 "
           />
         ))
       }
@@ -59,7 +91,7 @@ export default function MouseTailCircle() {
           transform: `translate(calc(-50% + ${clientX}px), calc(-50% + ${clientY}px))`,
           willChange: 'transform',
         }}
-        className="pointer-events-none w-160px h-160px rounded-full bg-transparent absolute top-0 left-0 border-2 border-white border-solid"
+        className="pointer-events-none w-160px h-160px rounded-full bg-opacity-30 bg-white absolute top-0 left-0 border-2 border-white border-solid"
       />
     </div>
   )
