e66a4d889a34131ca948d9e9a41d0e62a076bb36
pinky-pig <2545589993@qq.com>
2023-10-12 17:59:43 +0800

    fix: eslint code

--- src/components/ui/LocalMusicPlayer.tsx
+++ src/components/ui/LocalMusicPlayer.tsx
@@ -23,10 +23,16 @@ export default function LocalMusicPlayer() {
   })
 
   return (
-    <div className="absolute top-0 left-0 -z-1">
+    <div className="absolute top-0 right-0 z-9">
       <audio ref={audioRef}>
         <source src="/oh-my-love.mp3" type="audio/mpeg" />
       </audio>
+
+      <div className="relative w-40 h-40 rounded-full border-4 border-white border-solid box-border grid place-items-center">
+      {/* animate-[spin_10s_linear_infinite] */}
+        <div className="absolute top-0 left-0 -translate-1/2 w-[calc(100%_-_1rem)] h-[calc(100%_-_1rem)] rounded-full border-4 border-white border-dashed ">
+        </div>
+      </div>
     </div>
   )
 }
